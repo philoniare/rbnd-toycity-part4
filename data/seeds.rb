8448d8bb130d518b64b72f2f2ffd067a151b151a
philoniare <philoniare@gmail.com>
2016-05-17 13:12:55 -0400

    Prepared the master branch to start implementing methods

--- data/seeds.rb
+++ data/seeds.rb
@@ -1,8 +1,9 @@
 require 'faker'
 
-# This file contains code that populates the database with
-# fake data for testing purposes
-
 def db_seed
-  # Your code goes here!
+  # Use the faker gem to generate arrays with fake brands,
+  # product names, and prices.
+
+  # Create 10 new Product objects, and save them to the
+  # database
 end
